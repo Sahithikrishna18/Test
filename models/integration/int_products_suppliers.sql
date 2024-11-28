3e9a8f9fd9e75e06cb7597f29f2d44d8acd9685e
Sahithi Nalluri <sahithikrishna2017@gmail.com>
2024-11-28 19:50:52 +0000

    changes done

--- models/integration/int_products_suppliers.sql
+++ models/integration/int_products_suppliers.sql
@@ -4,18 +4,22 @@
 ) }}
 
 SELECT
+    -- Generate a surrogate key combining product_id and supplier_id
     MD5(CONCAT(p.product_id, '-', s.supplier_id)) AS surrogate_key,
     p.product_id,
     p.product_name,
     p.category,
     p.price,
     p.stock,
+    p.created_at,
     p.supplier AS supplier_name,
+    s.supplier_id,
+    s.contact_person,
     s.email AS supplier_email,
     s.phone AS supplier_phone,
     s.address AS supplier_address,
     s.country
-FROM stg_products p
-JOIN stg_suppliers s
+FROM {{ ref('stg_products') }} p
+JOIN {{ ref('stg_suppliers') }} s
     ON p.supplier = s.supplier_name
 WHERE p.stock > 0
